4a1ec465f340d82dc2e239b31aab84bf1c4f041b
Askbatguy <askbatguy@gmail.com>
2026-03-05 12:32:44 +0530

    Quartz sync: Mar 5, 2026, 12:32 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Batguy's NoteBook",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Lora",
+        body: "Montserrat",
         code: "IBM Plex Mono",
       },
       colors: {
