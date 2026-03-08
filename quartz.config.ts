8f6ef0992143685ea1b86f0f1f1a9767b13842a2
Askbatguy <askbatguy@gmail.com>
2026-03-08 16:52:37 +0530

    Quartz sync: Mar 8, 2026, 4:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://batguyblogs.github.io/Notes-host/",
+    baseUrl: "batguyblogs.github.io/Notes-host",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
